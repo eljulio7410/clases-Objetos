fddc76e5f80c8d9cdd2a27d95e4124c12549db8d
eljulio7410 <kaiser7420@gmail.com>
2025-05-14 15:30:31 -0500

    se añade atributo tipo y se imprime

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,10 +1,12 @@
 public class Estudiante {
+    String tipo;
     String nombre;
     String apellido;
     int edad;
     String carrera;
 
     public void info(){
+        System.out.println("tipo = " + tipo);
         System.out.println("nombre = " + nombre);
         System.out.println("apellido = " + apellido);
         System.out.println("edad = " + edad);
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Estudiante est = new Estudiante();
-        
+        est.tipo = "Estudiante";
         est.nombre = "Jose";
         est.apellido = "Suarez";
         est.edad = 21;
         est.carrera = "medicina";
 
+        Estudiante egresado = new Estudiante();
+        egresado.tipo = "Egresado";
+        egresado.nombre = "Juan";
+        egresado.apellido = "Flores";
+        egresado.edad = 21;
+        egresado.carrera = "psicologia";
+
         est.info();
+        System.out.println("-----------------------------");
+        egresado.info();
     }
 }
